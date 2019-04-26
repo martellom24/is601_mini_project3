e246507673ccf90bb4028315eee9eb9eb91ad836
martellom24 <martellom24@gmail.com>
2019-04-26 17:00:50 -0400

    Created Questions and seeded them into a questions table

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.14 on 2019-04-26 17:59:10.
+ * Generated for Laravel 5.8.14 on 2019-04-26 20:40:27.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
