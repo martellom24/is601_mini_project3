d4323259ac651b64eb2bb5d8abcc91eb12ff10d2
martellom24 <martellom24@gmail.com>
2019-04-26 16:19:58 -0400

    merge userProfile to master

---
+++ tests/Unit/ProfileTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use App\Profile;
+
+
+class profileTest extends TestCase
+{
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testSave()
+    {
+
+        $user = factory(User::class)->make();
+        $user->save();
+        $profile = factory(Profile::class)->make();
+        $profile->user()->associate($user);
+        $this->assertTrue($profile->save());
+
+    }
+}
