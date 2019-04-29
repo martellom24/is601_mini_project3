cbe0c9cb05112b9c9dd272cdb4f74b691443d432
martellom24 <martellom24@gmail.com>
2019-04-29 16:48:34 -0400

    aditions to master

--- resources/views/answer.blade.php
+++ resources/views/answer.blade.php
@@ -2,25 +2,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">Answer</div>
-                    <div class="card-body">
-                        {{$answer->body}}
-                    </div>
-                    <div class="card-footer">
-                        {{ Form::open(['method'  => 'DELETE', 'route' => ['answers.destroy', $question, $answer->id]])}}
-                        <button class="btn btn-danger float-right mr-2" value="submit" type="submit" id="submit">Delete
-                        </button>
-                        {!! Form::close() !!}
-                        <a class="btn btn-primary float-right" href="{{ route('answers.edit',['question_id'=> $question, 'answer_id'=> $answer->id, ])}}">
-                            Edit Answer
-                        </a>
+    <div id="app">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-2 bg-dark">
+                    @include('includes.sidebar')
+                </div>
+                <div class="col-10">
+                    <div class="row py-4">
+                        <div class="col-8">
+                            <div class="card">
+                                <div class="card-header">Answer</div>
+                                <div class="card-body">
+                                    {{$answer->body}}
+                                </div>
+                                <div class="card-footer">
+            {{--                        {{ Form::open(['method'  => 'DELETE', 'route' => ['answers.destroy', $question, $answer->id]])}}--}}
+                                    <button class="btn btn-danger float-right mr-2" value="submit" type="submit" id="submit">Delete
+                                    </button>
+            {{--                        {!! Form::close() !!}--}}
+                                    <a class="btn btn-primary float-right" href="#">
+                                        Edit Answer
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
